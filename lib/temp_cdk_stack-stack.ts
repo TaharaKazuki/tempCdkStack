fac7e8fc4a5fb304e6771af127a5729e3a2585b1
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-09-09 15:37:04 +0900

    update

--- lib/temp_cdk_stack-stack.ts
+++ lib/temp_cdk_stack-stack.ts
@@ -8,7 +8,9 @@ export class TempCdkStackStack extends cdk.Stack {
 
     // The code that defines your stack goes here
     new s3.Bucket(this, "MyFirstBucket", {
-      versioned: true
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
     })
   }
 }
